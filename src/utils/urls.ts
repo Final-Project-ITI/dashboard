873f9995081151c31d3d1fcbd08d0a84ae159658
waleedHAlmenawy <waleed.almenawy@outlook.com>
2024-06-27 11:07:31 +0300

    upload: last updatess

---
+++ src/utils/urls.ts
@@ -0,0 +1,34 @@
+/* Auth */
+export const REGISTER_URL = "/users/register";
+export const LOGIN_URL = "/authentication/login";
+export const USER_URL = "/user";
+export const REGISTER_CASHIER_URL = "/authentication/admin/register/cashier";
+
+/* Restaurants */
+export const RESTAURANT_URL = "/restaurant";
+export const RESTAURANTS_ADMINS_URL = "/user/restaurantsAdmins";
+export const RESTAURANTS_CAHSIERS_URL = "/user/restaurantCashiers";
+export const CREATE_RESTAURANT_URL = "/restaurant/authorization/register";
+
+/* Products */
+export const PRODUCT_URL = "/products";
+export const CREATE_PRODUCT_URL = "/products/admin";
+
+/* Categories */
+export const MENU_CATEGORY_URL = "/categories";
+
+/* Ingredients */
+export const INGREDIENT_URL = "/ingredients";
+
+/* Orders */
+export const GET_ORDERS_URL = "/orders/cashier";
+export const GET_ORDER_STATUESES = "/orderStatuses";
+
+/*ResturantCategory */
+export const RESTAURANT_CATEGORY_URL = "/restaurantCategory";
+
+/* deliveryman */
+export const DELIVERY_MAN_URL = "/deliveryman";
+
+/* delivery */
+export const DELIVERY_URL = "/delivery";
